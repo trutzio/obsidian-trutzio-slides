d7f2e5c6134ca46f6aace64c97ab74c56b3d4833
Christian Trutz <info@trutz-software-consulting.de>
2026-01-03 11:36:53 +0100

    refactor: simplify plugin structure by removing settings and updating manifest details

--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,22 @@
 import { Plugin } from "obsidian";
-import {
-	DEFAULT_SETTINGS,
-	SlidesPluginSettings,
-	SlidesSettingTab,
-} from "./settings.js";
 import { SlidesWebserver } from "./webserver.js";
 import path from "node:path";
 
 export default class SlidesPlugin extends Plugin {
-	settings: SlidesPluginSettings;
-
 	webserver: SlidesWebserver;
 
 	async onload() {
-		await this.loadSettings();
-
-		this.addRibbonIcon("info", "Show slides", (evt: MouseEvent) => {
-			const targetDocument = this.app.workspace.getActiveFile();
-			if (targetDocument) {
-				this.webserver.show(targetDocument.path);
-				window.open("http://localhost:3000");
+		this.addRibbonIcon(
+			"file-chart-pie",
+			"Show slides",
+			(evt: MouseEvent) => {
+				const targetDocument = this.app.workspace.getActiveFile();
+				if (targetDocument) {
+					this.webserver.show(targetDocument.path);
+					window.open("http://localhost:3000");
+				}
 			}
-		});
-
-		this.addSettingTab(new SlidesSettingTab(this.app, this));
+		);
 
 		const vaultDir = (
 			this.app.vault.adapter as unknown as { basePath: string }
@@ -38,17 +31,5 @@ export default class SlidesPlugin extends Plugin {
 
 	onunload() {
 		this.webserver.stop();
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign(
-			{},
-			DEFAULT_SETTINGS,
-			(await this.loadData()) as Partial<SlidesPluginSettings>
-		);
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
 	}
 }
